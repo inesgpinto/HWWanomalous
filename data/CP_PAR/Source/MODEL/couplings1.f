d5812e03e4dcd1b4bea4f6ba756793985c2fbda5
Ines Gales Alves Correia Pinto <ines.gales.alves.correia.pinto@cern.ch>
2023-06-15 15:52:45 +0100

    adding data

---
+++ data/CP_PAR/Source/MODEL/couplings1.f
@@ -0,0 +1,39 @@
+ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+c      written by the UFO converter
+ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+
+      SUBROUTINE COUP1()
+
+      IMPLICIT NONE
+      INCLUDE 'model_functions.inc'
+
+      DOUBLE PRECISION PI, ZERO
+      PARAMETER  (PI=3.141592653589793D0)
+      PARAMETER  (ZERO=0D0)
+      INCLUDE 'input.inc'
+      INCLUDE 'coupl.inc'
+      GC_3 = -(MDL_EE*MDL_COMPLEXI)
+      GC_5 = MDL_EE__EXP__2*MDL_COMPLEXI
+      GC_97 = (MDL_EE__EXP__2*MDL_COMPLEXI)/(2.000000D+00
+     $ *MDL_STH__EXP__2)
+      GC_125 = -((MDL_EE*MDL_COMPLEXI)/(MDL_STH*MDL_SQRT__2))
+      GC_137 = -((MDL_CTH*MDL_EE*MDL_COMPLEXI)/MDL_STH)
+      GC_138 = (-2.000000D+00*MDL_CTH*MDL_EE__EXP__2*MDL_COMPLEXI)
+     $ /MDL_STH
+      GC_260 = -6.000000D+00*MDL_COMPLEXI*MDL_LAM*MDL_VEVHAT
+      GC_266 = (4.000000D+00*MDL_CHW*MDL_COMPLEXI*MDL_VEVHAT)
+     $ /MDL_LAMBDASMEFT__EXP__2
+      GC_272 = (4.000000D+00*MDL_CHW*MDL_EE*MDL_COMPLEXI*MDL_VEVHAT)
+     $ /MDL_LAMBDASMEFT__EXP__2
+      GC_276 = (-4.000000D+00*MDL_CHW*MDL_EE__EXP__2*MDL_COMPLEXI
+     $ *MDL_VEVHAT)/MDL_LAMBDASMEFT__EXP__2
+      GC_283 = (MDL_EE__EXP__2*MDL_COMPLEXI*MDL_VEVHAT)/(2.000000D+00
+     $ *MDL_STH__EXP__2)
+      GC_320 = (4.000000D+00*MDL_CHW*MDL_CTH*MDL_COMPLEXI*MDL_STH
+     $ *MDL_VEVHAT)/MDL_LAMBDASMEFT__EXP__2
+      GC_328 = (4.000000D+00*MDL_CHW*MDL_COMPLEXI*MDL_STH__EXP__2
+     $ *MDL_VEVHAT)/MDL_LAMBDASMEFT__EXP__2
+      GC_451 = -((MDL_COMPLEXI*MDL_YB)/MDL_SQRT__2)
+      GC_219 = (4.000000D+00*MDL_CHW*MDL_COMPLEXI*MDL_STH__EXP__2)
+     $ /MDL_LAMBDASMEFT__EXP__2
+      END
